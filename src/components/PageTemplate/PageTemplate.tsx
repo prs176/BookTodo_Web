03ff9c52d43bb94f4b5275e99e20b3ae1dd689fe
prs176 <prsx176@gmail.com>
2022-04-05 19:38:31 +0900

    [ADD] page template

---
+++ src/components/PageTemplate/PageTemplate.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const PageTemplateContents = styled.div`
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+  & > * {
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
